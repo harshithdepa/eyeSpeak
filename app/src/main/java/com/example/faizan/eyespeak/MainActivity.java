f728c2b973e9841bdbdb72c1c22980708227615d
theyeeman <fernando_y767@hotmail.com>
2017-03-11 21:39:00 -0500

    Added PlayerActivity

--- app/src/main/java/com/example/faizan/eyespeak/MainActivity.java
+++ app/src/main/java/com/example/faizan/eyespeak/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.faizan.eyespeak;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playButton = (Button) findViewById(R.id.playButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toPlayerActivity = new Intent(MainActivity.this, PlayerActivity.class);
+                MainActivity.this.startActivity(toPlayerActivity);
+            }
+        });
+
     }
 }
